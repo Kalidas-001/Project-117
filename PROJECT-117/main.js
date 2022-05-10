c07be64359aa8230a568aa6658888392ab78f7fe
Kalidas-001 <89841885+Kalidas-001@users.noreply.github.com>
2022-05-10 16:59:15 +0530

    Add files via upload

---
+++ PROJECT-117/main.js
@@ -0,0 +1,18 @@
+quick_draw_data_set= ["pen","paper","book","bottle"] 
+
+var random_number = 0;
+
+ random_number = Math.floor((Math.random()*array_1.length)+1)
+
+time_counter = 0 ;
+
+timer_check ="";
+
+drawn_sketch ="";
+
+answer_holder ="";
+
+score = 0;
+
+
+ 
